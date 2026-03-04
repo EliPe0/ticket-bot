62b87274afc5029d2a9e9534aa9371932439326a
EliPe0 <filipe10nogueira@gmail.com>
2026-03-04 14:46:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config({quiet: true});
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+    ]
+})
+
+client.once('clientReady', () => {
+    console.log(`🚀 | Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN);
